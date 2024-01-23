64b3a1dbb75a27d71152ef4716292f141c8e3aac
muyi <muyiorlk@gmail.com>
2024-01-23 23:37:58 +0800

    finish cpp lab2

---
+++ Code/SUSTechCPP/src/chapter2/lab2ex2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    long a = 56789;
+    long b = 23456789;
+    long c = a * b;
+
+    cout << c <<endl;
+    cout << sizeof(long) << endl;
+    cout << sizeof(int) << endl;
+}
